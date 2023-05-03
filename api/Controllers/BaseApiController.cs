182d6f8347815eae0f94ab73b9eeac73143554bd
EmanuelPrgic <emanuel.prgic@fpmoz.sum.ba>
2023-05-03 14:04:46 +0200

    Added Login and Register

---
+++ api/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
